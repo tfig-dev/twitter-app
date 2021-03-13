461b306427cf5827d96bcff8e580481a735cd526
Tiago Figueiredo <tiagofr.figueiredo@gmail.com>
2021-03-13 20:18:36 +0000

    fifth part -(correction) about page & routes

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -2,7 +2,7 @@
   <q-page class="q-pa-lg">
     <h4 class="q-mt-none q-mb-md text-weight-bold">About Britter</h4>
     <div class="text-body1">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptatum quis perspiciatis possimus dicta eius harum optio consequuntur in eum, at tempore numquam voluptate modi sequi suscipit omnis sed odio?</p>
+          <p>Lorem ipsum dolor sit ametconsectetur adipisicing elit. Sapiente voluptatum quis perspiciatis possimus dicta eius harum optio consequuntur in eum, at tempore numquam voluptate modi sequi suscipit omnis sed odio?</p>
     </div>
   </q-page>
 </template>
