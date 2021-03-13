13c644436c4e82008eef6c4902573a72ee7a6f32
Tiago Figueiredo <tiagofr.figueiredo@gmail.com>
2021-03-13 19:38:09 +0000

    second part - righ nav

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -38,7 +38,37 @@
     </q-drawer>
 
     <q-drawer show-if-above v-model="right" side="right" bordered>
-      <!-- drawer content -->
+       <q-input placeholder="Search Britter" class="q-ma-md" outlined rounded dense>
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+      <q-list separator padding>
+       <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-grey">TECH</q-item-label>
+          <q-item-label class="text-weight-bold">What!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+      <q-item class="q-pa-md">
+        <q-item-section>
+          <q-item-label overline class="text-grey">Sports</q-item-label>
+          <q-item-label class="text-weight-bold">What!</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      </q-list>
+
     </q-drawer>
 
     <q-page-container>
